f66c0fcdff966bc0c056ada9f09323885882cc99
jah1603 <hendersonjamesalexander@gmail.com>
2019-01-24 10:35:43 +0000

    GlobalButton.js created and integrated into Landing.js
    Co-authored-by: David Pears <davidapears@users.noreply.github.com>
    Co-authored-by: Stephen Rooney <SFR1981@users.noreply.github.com>

--- components/GlobalButton.js
+++ components/GlobalButton.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react'
 import { Button } from 'react-native-elements';
-import { View } from 'react-native';
+import { View, Dimensions, StyleSheet } from 'react-native';
 
 
-export default class Button extends Component {
+export default class GlobalButton extends Component {
+
+  returnButtonTitle(){
+    return this.props.buttonTitle
+  }
 
   render() {
     return (
 
-      <View style={buttonContainerStyle.content}>
+
 
               <Button
                 onPress={this.props.onPress}
-                textStyle={{fontSize: 25, color: 'black'}}
-                title=`${this.props.buttonTitle}`
+                textStyle={{fontSize: 19, color: 'black'}}
+                title={this.props.buttonTitle}
                 titleStyle={{  }}
                 buttonStyle={{
                   backgroundColor: "pink",
-                  width: 175,
+                  marginLeft: this.props.marginLeft ? this.props.marginLeft : 0,
+                  marginRight: this.props.marginRight ? this.props.marginRight : 0,
+                  width: Dimensions.get('window').width*0.3,
                   height: 55,
-                  borderRadius: 20
+                  borderRadius: 10,
+
                 }}
                 containerStyle={{ marginTop: 20 }}
               />
 
 
-      </View>
+
 
     )
   }
@@ -34,7 +41,7 @@ export default class Button extends Component {
 const buttonContainerStyle = StyleSheet.create({
   content: {
     width: '100%',
-    backgroundColor: 'transparent',
+    backgroundColor: 'purple',
     height: 100,
     marginBottom: 20,
     justifyContent: 'center',
--- containers/Global/Landing.js
+++ containers/Global/Landing.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Image, Dimensions, Button, Text, View } from 'react-native';
+import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 import { Constants } from 'expo'
 import NavBar from '../../components/NavBar.js';
+import GlobalButton from '../../components/GlobalButton.js';
 import AutoHeightImage from 'react-native-auto-height-image';
 
 export default class Landing extends React.Component {
@@ -18,17 +19,10 @@ export default class Landing extends React.Component {
       <AutoHeightImage source={require('../../assets/AppIcons/transparentlogo.png')} style={{marginTop: Constants.statusBarHeight + Dimensions.get('window').height*0.18}} width={Dimensions.get('window').width*0.77} />
 
       <View style={landingStyle.iconsContainer}>
-      <Button
-        title="Register"
-        style={{marginLeft: Dimensions.get('window').width*0.035, marginRight: Dimensions.get('window').width*0.035}}
-        onPress={() => navigate('Register', {name: 'Home'})}
-      />
 
-      <Button
-        style={{marginLeft: Dimensions.get('window').width*0.035, marginRight: Dimensions.get('window').width*0.035}}
-        title="Sign in"
-        onPress={() => navigate('SignIn', {name: 'Home'})}
-      />
+      <GlobalButton marginLeft={Dimensions.get('window').width*0.12} onPress={() => navigate('Register', {name: 'Home'})} buttonTitle={"Register"} />
+      <GlobalButton marginRight={Dimensions.get('window').width*0.20} onPress={() => navigate('Sign in', {name: 'Home'})} buttonTitle={"Sign in"} />
+
       </View>
 
       <View style={landingStyle.descriptionContainer}>
@@ -56,8 +50,9 @@ const landingStyle = StyleSheet.create({
   },
 
   iconsContainer: {
-    width: Dimensions.get('window').width*0.5,
-    backgroundColor: 'white',
+    width: Dimensions.get('window').width,
+    marginLeft: 0,
+    backgroundColor: 'transparent',
     justifyContent: 'space-between',
     alignItems: 'center',
     flexDirection: 'row',
