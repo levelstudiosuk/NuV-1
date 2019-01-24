63acd59e9ddd67813f856158ef0c927ba7d79813
jah1603 <hendersonjamesalexander@gmail.com>
2019-01-24 10:04:51 +0000

    Description text View for Landing.js
    Co-authored-by: David Pears <davidapears@users.noreply.github.com>
    Co-authored-by: Stephen Rooney <SFR1981@users.noreply.github.com>

--- containers/Global/Landing.js
+++ containers/Global/Landing.js
@@ -15,7 +15,7 @@ export default class Landing extends React.Component {
 
     return (
       <View style={landingStyle.container}>
-      <AutoHeightImage source={require('../../assets/AppIcons/transparentlogo.png')} style={{marginTop: Constants.statusBarHeight + Dimensions.get('window').height*0.25}} width={Dimensions.get('window').width*0.77} />
+      <AutoHeightImage source={require('../../assets/AppIcons/transparentlogo.png')} style={{marginTop: Constants.statusBarHeight + Dimensions.get('window').height*0.18}} width={Dimensions.get('window').width*0.77} />
 
       <View style={landingStyle.iconsContainer}>
       <Button
@@ -29,7 +29,10 @@ export default class Landing extends React.Component {
         title="Sign in"
         onPress={() => navigate('SignIn', {name: 'Home'})}
       />
+      </View>
 
+      <View style={landingStyle.descriptionContainer}>
+        <Text style={landingStyle.descriptionText}>DESCRIPTION: At vero eos et aos ducimupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod max</Text>
       </View>
 
       </View>
@@ -44,6 +47,14 @@ const landingStyle = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  descriptionContainer: {
+    marginTop: Dimensions.get('window').height*0.1
+  },
+  descriptionText: {
+    paddingLeft: Dimensions.get('window').width*0.115,
+    paddingRight: Dimensions.get('window').width*0.115
+  },
+
   iconsContainer: {
     width: Dimensions.get('window').width*0.5,
     backgroundColor: 'white',
