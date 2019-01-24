7e5ee2a661b4658d53058576473a32d0e2cc1e3f
jah1603 <hendersonjamesalexander@gmail.com>
2019-01-24 18:26:11 +0000

    Sticky footer rendered and inner content scrollable
    Co-authored-by: David Pears <davidapears@users.noreply.github.com>
    Co-authored-by: Stephen Rooney <SFR1981@users.noreply.github.com>

--- containers/Global/Home.js
+++ containers/Global/Home.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { StyleSheet, Platform, Dimensions, Button, Text, View } from 'react-native';
+import { StyleSheet, Platform, ScrollView, Dimensions, Button, Text, View } from 'react-native';
 import { Constants } from 'expo'
 import * as TimeGreeting from '../../helper_functions/TimeGreeting.js';
 import NavBar from '../../components/NavBar.js';
 import AutoHeightImage from 'react-native-auto-height-image';
 import GlobalButton from '../../components/GlobalButton.js';
+import StickyHeaderFooterScrollView from 'react-native-sticky-header-footer-scroll-view';
 
 export default class Home extends React.Component {
   static navigationOptions = {
     title: null,
     headerTitle: (
-     <AutoHeightImage width={75} style={{position: 'absolute', right: Platform.OS === 'android' ? 0 : -65 }} source={require('../../assets/AppIcons/transparentlogo.png')}/>
+      <AutoHeightImage width={75} style={{position: 'absolute', right: Platform.OS === 'android' ? 0 : -65 }} source={require('../../assets/AppIcons/transparentlogo.png')}/>
  ),
 }
 
@@ -19,24 +20,39 @@ export default class Home extends React.Component {
 
     return (
       <View style={homeStyle.container}>
-      <View style={homeStyle.buttonContainer}>
-        <Text style={{fontSize: 18, color: 'midnightblue'}}>{TimeGreeting.getTimeBasedGreeting("Jarrod")} </Text>
-      </View>
-      <View style={homeStyle.iconsContainer}>
 
-      <GlobalButton marginLeft={Dimensions.get('window').width*0.12} onPress={() => navigate('SignIn', {name: 'Home'})} buttonTitle={"Recipes"} />
-      <GlobalButton marginRight={Dimensions.get('window').width*0.12} onPress={() => navigate('Register', {name: 'Home'})} buttonTitle={"Eateries"} />
+      <StickyHeaderFooterScrollView
+      makeScrollable={true}
+      renderStickyHeader={() => ( <View></View> )}
+      renderStickyFooter={() => (
+        <View style={{alignItems: 'center'}}>
+          <NavBar navigation={this.props.navigation} />
+        </View>
+      )}
+    >
+    <View style={homeStyle.buttonContainer}>
+    <AutoHeightImage width={Dimensions.get('window').width*0.5} style={{borderRadius: Dimensions.get('window').width*0.25 }} source={require('../../assets/wil.jpg')}/>
+    </View>
 
-      </View>
+    <View style={homeStyle.greetingContainer}>
+      <Text style={{fontSize: 20, color: 'black'}}>{TimeGreeting.getTimeBasedGreeting("Wil Cornish")} </Text>
+    </View>
 
-      <View style={homeStyle.iconsContainer}>
+    <View style={homeStyle.iconsContainer}>
 
-      <GlobalButton marginLeft={Dimensions.get('window').width*0.12} onPress={() => navigate('SignIn', {name: 'Home'})} buttonTitle={"Brands"} />
-      <GlobalButton marginRight={Dimensions.get('window').width*0.12} onPress={() => navigate('Register', {name: 'Home'})} buttonTitle={"Media"} />
+    <GlobalButton marginLeft={Dimensions.get('window').width*0.12} onPress={() => navigate('SignIn', {name: 'Home'})} buttonTitle={"Recipes"} />
+    <GlobalButton marginRight={Dimensions.get('window').width*0.12} onPress={() => navigate('Register', {name: 'Home'})} buttonTitle={"Eateries"} />
 
-      </View>
+    </View>
 
-      <NavBar navigation={this.props.navigation} />
+    <View style={homeStyle.iconsContainer}>
+
+    <GlobalButton marginLeft={Dimensions.get('window').width*0.12} onPress={() => navigate('SignIn', {name: 'Home'})} buttonTitle={"Brands"} />
+    <GlobalButton marginRight={Dimensions.get('window').width*0.12} onPress={() => navigate('Register', {name: 'Home'})} buttonTitle={"Media"} />
+
+    </View>
+    </StickyHeaderFooterScrollView>
+
       </View>
     );
   }
@@ -59,6 +75,15 @@ const homeStyle = StyleSheet.create({
     flexDirection: 'row',
   },
   buttonContainer: {
-    marginBottom: Dimensions.get('window').height*0.01
-  }
+    marginBottom: Dimensions.get('window').height*0.01,
+    marginTop: Dimensions.get('window').height*0.05,
+    alignItems: 'center',
+  },
+  greetingContainer: {
+    marginBottom: Dimensions.get('window').height*0.01,
+    marginTop: Dimensions.get('window').height*0.02,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    height: Dimensions.get('window').height*0.03
+      }
 });
