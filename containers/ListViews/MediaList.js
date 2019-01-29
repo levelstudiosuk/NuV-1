3e04ffddd538a498053c72be5a061bc8914851eb
David A Pears <davidapears@gmail.com>
2019-01-29 12:36:29 +0000

    ADDS (hardcoded) media/news item to show layout, to be made dynamic once backend is connected
    Co-authored-by: Stephen Rooney <SFR1981@users.noreply.github.com>Co-authored-by: James Henderson <jah1603@users.noreply.github.com>

--- containers/ListViews/MediaList.js
+++ containers/ListViews/MediaList.js
@@ -22,7 +22,7 @@ export default class MediaList extends React.Component {
     }
 
     render() {
-    
+      const {navigate} = this.props.navigation;
       return (
 
     <View style={mediaListStyle.container}>
@@ -36,7 +36,7 @@ export default class MediaList extends React.Component {
       <View style={{flex: 1, flexDirection: 'row'}}>
         <SmallTwoWayToggle/>
         <AddItemButton navigation={this.props.navigation} />
-        <FaveButton navigation={this.props.navigation}/>
+        {/*<FaveButton navigation={this.props.navigation}/>*/}
       </View>
 
       <AutoHeightImage
@@ -46,17 +46,31 @@ export default class MediaList extends React.Component {
       />
 
       <Text style={{fontSize: 18, textAlign: 'center'}}>
-          [Good morning] [User_name]; here are all the news articles.{"\n"}{"\n"}
+          [Good morning] [User_name]; here is the news{"\n"}{"\n"}
       </Text>
 
       <View style={{marginTop: Dimensions.get('window').height*0.04}}>
       </View>
 
-      <View>
-      <Text>Hello world</Text>
-      </View>
+      <View style={mediaListStyle.mediaitem}>
+      <TouchableHighlight style={mediaListStyle.mediaimage}>
+        <Image source={require('../../assets/AppIcons/newsdefault.png')} style={{height: 100, width: 100}}/>
+      </TouchableHighlight>
+          <View style={mediaListStyle.mediatextcontainer}>
+            <View>
+              <Text style={mediaListStyle.mediatitle}>
+              Papa John’s Vegan Pizza Launching 28.1.19
+              </Text>
+            </View>
+            <View>
+              <Text style={mediaListStyle.mediadescription}>
+              After PETA’s successful online petition, which gained over 29,000 signatures requesting a vegan option, Papa John’s have announced that they are adding Sheese to its nationwide menu.
+              </Text>
+            </View>
+          </View>
+        </View>
 
-      <View style={mediaListStyle.submitContainer}>
+      <View >
         <GlobalButton
           buttonTitle="Home"
           onPress={() => navigate('Home', {name: 'Home'})}/>
@@ -80,10 +94,29 @@ const mediaListStyle = StyleSheet.create({
     marginBottom: Platform.OS === 'ios' ? Dimensions.get('window').height*0.05 : Dimensions.get('window').height*0.05
   },
   header: {
-    fontSize: 24,
-    color: 'green',
     textAlign: 'center',
     marginTop:  Constants.statusBarHeight+10,
     marginBottom: Dimensions.get('window').height*0.01
   },
+
+
+  mediaitem: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingBottom: 20,
+  },
+  mediatextcontainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  mediatitle: {
+    color: '#0dc6b5',
+    margin: 4,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  mediadescription: {
+    margin: 4,
+    fontSize: 15
+  }
 });
