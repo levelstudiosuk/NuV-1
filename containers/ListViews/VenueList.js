989593c058f7ef8a5e34fe21ec8990f372019585
jah1603 <hendersonjamesalexander@gmail.com>
2019-02-06 14:22:00 +0000

    All venues from Heroku API are populating the venue list container
    Co-authored-by: David Pears <davidapears@users.noreply.github.com>
    Co-authored-by: Stephen Rooney <SFR1981@users.noreply.github.com>

--- containers/ListViews/VenueList.js
+++ containers/ListViews/VenueList.js
@@ -54,7 +54,7 @@ export default class VenueList extends React.Component {
     mapVenueItems(){
       const {navigate} = this.props.navigation;
 
-      return this.state.venueItems((item, i) =>
+      return this.state.venueItems.map((item, i) =>
 
       <View key={i} style={venueListStyle.venueitem}>
       <TouchableHighlight
@@ -131,39 +131,10 @@ export default class VenueList extends React.Component {
       <View style={{marginTop: Dimensions.get('window').height*0.04}}>
       </View>
 
-      <View style={venueListStyle.venueitem}>
-      <TouchableHighlight
-        style={venueListStyle.venueimage}
-        onPress={() => navigate('VenueView', {title: this.state.items[0].title, address: this.state.items[0].address, description: this.state.items[0].description, type: this.state.items[0].type, image: this.state.items[0].image})}>
-        <Image source={require('../../assets/AppIcons/venuedefault.png')} style={{height: 75, width: 75}}/>
-      </TouchableHighlight>
-          <View style={venueListStyle.venuetextcontainer}>
-            <View>
-              <Text
-              style={venueListStyle.venuetitle}
-              onPress={() => navigate('VenueView', {title: this.state.items[0].title, address: this.state.items[0].address, description: this.state.items[0].description, type: this.state.items[0].type, image: this.state.items[0].image})}>
-              Hendersons Vegan Restaurant
-              </Text>
-              <Text
-              style={venueListStyle.venuetype}
-              onPress={() => navigate('VenueView', {title: this.state.items[0].title, address: this.state.items[0].address, description: this.state.items[0].description, type: this.state.items[0].type, image: this.state.items[0].image})}>
-              Cafe
-              </Text>
-            </View>
-            <View>
-              <Text
-              style={venueListStyle.venuedescription}
-              onPress={() => navigate('VenueView', {title: this.state.items[0].title, address: this.state.items[0].address, description: this.state.items[0].description, type: this.state.items[0].type, image: this.state.items[0].image})}>
-              Long-running vegetarian deli/eatery showcasing local and organic produce, plus regular live music.
-              </Text>
-              <Text
-              style={venueListStyle.venueaddress}
-              onPress={() => navigate('VenueView', {title: this.state.items[0].title, address: this.state.items[0].address, description: this.state.items[0].description, type: this.state.items[0].type, image: this.state.items[0].image})}>
-              25c Thistle St, Edinburgh EH2 1DX
-              </Text>
-            </View>
-          </View>
-        </View>
+      {this.state.venueItems ? (
+        this.mapVenueItems()
+      ): null
+    }
 
       <View >
         <GlobalButton
