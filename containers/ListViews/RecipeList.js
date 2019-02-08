3ffb3b0994a91ef62db0f9ae0b27e824454fc75c
jah1603 <hendersonjamesalexander@gmail.com>
2019-02-08 13:31:04 +0000

    Set underlay color of TouchableOpacity in RecipeList.js to white
    Co-authored-by: David Pears <davidapears@users.noreply.github.com>
    Co-authored-by: Stephen Rooney <SFR1981@users.noreply.github.com>

--- containers/ListViews/RecipeList.js
+++ containers/ListViews/RecipeList.js
@@ -273,6 +273,7 @@ export default class RecipeList extends React.Component {
           <View style={{alignItems: 'center', marginTop: Dimensions.get('window').height*0.20, height: Dimensions.get('window').height*0.6, width: Dimensions.get('window').width}}>
 
           <TouchableHighlight
+          underlayColor="white"
           onPress={() => navigate('RecipeView', {token: this.props.navigation.getParam('token', 'NO-ID'), id: this.state.activeItem.item.id, name: this.state.activeItem.item.title, prep_time: this.state.activeItem.item.prep_time, cook_time: this.state.activeItem.item.cook_time, image: this.state.activeItem.item.image})}
           style={underlayColor="white"}
           >
